2fcf5babbd4d34801f59020d3adaad3780c6f5e7
Jay Parikh <jay.parikh@emxcelsolutions.com>
2017-12-06 19:03:09 +0530

    second

--- app/src/main/java/com/example/emxcel/myapplication/MainActivity.java
+++ app/src/main/java/com/example/emxcel/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("this is main activity");
+        System.out.println("this is main activity 1");
     }
 }
